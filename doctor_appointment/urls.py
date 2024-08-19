d981487deeff39382aa8d7b5c28942a7dff51767
AKASHREDDII <98142273+AKASHREDDII@users.noreply.github.com>
2024-08-19 13:40:17 +0530

    urls.py

--- doctor_appointment/urls.py
+++ doctor_appointment/urls.py
@@ -1,6 +1,7 @@
-"""doctor_appointment URL Configuration
+"""
+doctor_appointment URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information, please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
@@ -13,12 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("base.urls"))
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
